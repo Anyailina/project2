2458d562a3d1f7d40605e7abcff54fd485f4fda1
Ann <annill13@mail.ru>
2024-09-26 14:05:45 +0300

    add logging

---
+++ src/main/java/org/example/project2/interceptor/InterceptorConfiguration.java
@@ -0,0 +1,23 @@
+package org.example.project2.interceptor;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfigurer {
+
+
+    private LoggingInterceptor logInterceptor;
+
+    public InterceptorConfiguration(LoggingInterceptor logInterceptor) {
+        this.logInterceptor = logInterceptor;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+    }
+}
